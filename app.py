5503da9478624ececde3be19ccd6ff9026a47f6c
Colbin Gutierrez <colbing11@outlook.com>
2025-07-09 20:08:32 -0500

    Add logo to assets folder

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title = "Badger Den RV Park", layout = "wide")
+
+# HEADER SECTION #
+with st.container():
+    st.subheader("Bagder Den ,oloRV Park")
+    st.title("Welcome to Badger Den RV Park!")
+    st.write("Welcome! Badger Den RV Park will be glad to accomodate your stay.")
+
+# ABOUT US #
+with st.container():
+    st.write("---")
